348638f6635cf937a70785e9bd425ea0e5685279
Matcham89 <cjmatcham@hotmail.co.uk>
2022-11-16 09:27:27 +0000

    updates

--- three-months-later/iac/main.tf
+++ three-months-later/iac/main.tf
@@ -37,23 +37,23 @@ module "vpc" {
         ]
     }
 
-#     routes = [
-#         {
-#             name                   = "egress-internet"
-#             description            = "route through IGW to access internet"
-#             destination_range      = "0.0.0.0/0"
-#             tags                   = "egress-inet"
-#             next_hop_internet      = "true"
-#         },
-#         {
-#             name                   = "app-proxy"
-#             description            = "route through proxy to reach app"
-#             destination_range      = "10.50.10.0/24"
-#             tags                   = "app-proxy"
-#             next_hop_instance      = "app-proxy-instance"
-#             next_hop_instance_zone = "us-west1-a"
-#         },
-#     ]
+    routes = [
+        {
+            name                   = "egress-internet"
+            description            = "route through IGW to access internet"
+            destination_range      = "0.0.0.0/0"
+            tags                   = "egress-inet"
+            next_hop_internet      = "true"
+        },
+        {
+            name                   = "app-proxy"
+            description            = "route through proxy to reach app"
+            destination_range      = "10.50.10.0/24"
+            tags                   = "app-proxy"
+            next_hop_instance      = "app-proxy-instance"
+            next_hop_instance_zone = "us-west1-a"
+        },
+    ]
 }
 
 
@@ -77,7 +77,7 @@ module "gke" {
   region                     = var.cluster_region
   zones                      = var.zonal_regions
   network                    = module.vpc.network_name
-  subnetwork                 = 
+  subnetwork                 = ""
   ip_range_pods              = "us-central1-01-gke-01-pods"
   ip_range_services          = "us-central1-01-gke-01-services"
   http_load_balancing        = false
